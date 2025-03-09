b988ac6e097212ca1850a9838e1aa7d24cf074a7
zzhakerMikael <mihailpokidov2008@yandex.ru>
2025-03-09 21:17:07 +0700

    chang1

---
+++ bot.py
@@ -0,0 +1,35 @@
+import telebot
+from bot_logic import gen_pass, gen_emodji, flip_coin  # Импортируем функции из bot_logic
+
+# Замени 'TOKEN' на токен твоего бота
+bot = telebot.TeleBot("7764692293:AAG1vysoEkhy1391q_gQ-LSzEKX6EJ6zxVw")
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши команду /hello, /bye, /pass, /emodji или /coin  ")
+
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
+
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
+
+@bot.message_handler(commands=['pass'])
+def send_password(message):
+    password = gen_pass(10)  # Устанавливаем длину пароля, например, 10 символов
+    bot.reply_to(message, f"Вот твой сгенерированный пароль: {password}")
+
+@bot.message_handler(commands=['emodji'])
+def send_emodji(message):
+    emodji = gen_emodji()
+    bot.reply_to(message, f"Вот эмоджи': {emodji}")
+
+@bot.message_handler(commands=['coin'])
+def send_coin(message):
+    coin = flip_coin()
+    bot.reply_to(message, f"Монетка выпала так: {coin}")
+
+# Запускаем бота
+bot.polling()
